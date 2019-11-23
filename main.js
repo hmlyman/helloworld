d5214693707d9bda17bb67d4d357df0f8e8a395d
hmlyman <56166746+hmlyman@users.noreply.github.com>
2019-11-23 12:28:49 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 let comicData;
 let submit = document.getElementById("submit");
+let url = "https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json";
+let request = new XMLHttpRequest();
 
 function loadData() {
-    let url = "https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json";
-    let request = new XMLHttpRequest();
-
     request.open("GET", url, true);
     request.onload = loadComplete;
     request.send();
@@ -13,24 +12,26 @@ function loadData() {
 function loadComplete() {
     comicData = JSON.parse(this.responseText);
     console.log(comicData);
+    document.getElementById("title").innerHTML = comicData.title;
     document.getElementById("comic").src = comicData.img;
     document.getElementById("altText").innerHTML = comicData.alt;
-    document.getElementById("title").innerHTML = comicData.title;
     document.getElementById("published").innerHTML = "Published "+ comicData.month +"-"+ comicData.day +"-"+ comicData.year;
 }
 
 loadData();
 
-
 function submitNumber() {
-    let comicNumber = document.getElementById("input").value;
     console.log("Submit test");
+    let comicNumber = document.getElementById("input").value;
 
     if (comicNumber <= comicData.num){
-        console.log("Valid number entered")
+        console.log("Valid number entered");
+        url = "https://cors-anywhere.herokuapp.com/http://xkcd.com/"+comicNumber+"/info.0.json"
+        loadData();
     }
     else if (comicNumber > comicData.num){
         console.log ("Number entered is too high.");
+        throw{ name:"ComicNumberTooHigh", message:"The comic number entered is too high, please enter a lower number."};
     }
 
 }
